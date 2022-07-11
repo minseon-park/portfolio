96aca14a5a87ea02fff07cd4002dce53f6378bcd
minseon <minseonpark00@gmail.com>
2022-07-11 15:17:07 +0900

    change about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,24 @@
 import "../styles/App.css";
 import "../styles/About.css";
-import { aboutTitle, minseonInfo, educationInfo, channelInfo, contactInfo } from "../data";
-import Text from "./Text";
+import { minseonInfo, educationInfo, channelInfo, contactInfo } from "../data";
 import TextBox from "./TextBox";
 export default function About() {
   return (
     <div className="about">
-      {/* TODO: 타이틀 . 색깔 바꾸기 */}
-      <Text textData={aboutTitle} classname="aboutTitle" />
+      <div className="aboutTitle">
+        <p>About Me </p>
+        <p id="aboutColor"> .</p>
+      </div>
       <div className="aboutContainer">
         <img className="aboutImg" alt="profilePic" src="images/Profile.png"></img>
         <div className="infoContainer">
           <div className="firstRow">
-            <TextBox titleText="I'm Minseon" textData={minseonInfo} classname="minseonInfo" />
-            <TextBox titleText="Education" textData={educationInfo} classname="minseonInfo" />
+            <TextBox titleText="I'm Minseon" textData={minseonInfo} classname="aboutInfo" />
+            <TextBox titleText="Education" textData={educationInfo} classname="aboutInfo" />
           </div>
           <div className="secondRow">
-            <TextBox titleText="Contact" textData={contactInfo} classname="minseonInfo" />
-            <TextBox titleText="Channel" textData={channelInfo} classname="minseonInfo" />
+            <TextBox titleText="Contact" textData={contactInfo} classname="aboutInfo" />
+            <TextBox titleText="Channel" textData={channelInfo} classname="aboutInfo" />
           </div>
         </div>
       </div>
--- src/data.ts
+++ src/data.ts
@@ -5,7 +5,6 @@ export const introText = [
   "저는 무언가 만드는것을 좋아하고 새로운 도전을 즐기고 두려워하지 않습니다.",
   "제가 만들고있는 것들. 도전하고 있는 것들. 이곳에서 마음껏 구경해주세요",
 ];
-export const aboutTitle = ["About Me", "."];
 export const minseonInfo = ["박민선 / Minseon Park", "1997.12.19", "서울특별시 종로구"];
 export const educationInfo = ["San Francisco State University 졸업"];
 export const contactInfo = ["minseonpark00@gmail.com", "010-7695-5277"];
